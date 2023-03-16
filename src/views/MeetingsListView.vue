23492ec918aba4eb4d43ebaf11f7d1f09c45f089
Oliver Burgmaier <oliver.burgmaier@gmail.com>
2023-03-16 21:22:51 +0100

    Delete dialog added

--- src/views/MeetingsListView.vue
+++ src/views/MeetingsListView.vue
@@ -29,29 +29,64 @@
               icon="mdi-delete"
               variant="text"
               density="compact"
-              @click="deleteMeeting(meeting.id)"
+              @click="deleteMeetingId=meeting.id; dialogDelete=true"
             ></v-btn>
           </template>
       </v-list-item>
     </v-list>
+
+    <!-- Detelte Dialog -->
+    <v-dialog
+      v-model="dialogDelete"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Delete Meeting?
+        </v-card-title>
+        <v-card-text>Do you want to delete this meeting?</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="orange-darken-4"
+            text
+            @click="dialogDelete = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="orange-darken-1"
+            text
+            @click="deleteMeeting(deleteMeetingId)"
+          >
+            Delete
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-card>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useMeetingStore } from '@/stores/MeetingStore.js'
 const store = useMeetingStore()
 
 const router = useRouter()
 
+const dialogDelete = ref(false)
+const deleteMeetingId = -1;
+
 const openMeeting = (meetingId) => {
-  router.push(`/timer/${meetingId}`);
+  router.push(`/timer/${meetingId}`)
 }
 
 const deleteMeeting = (meetingId) => {
   store.deleteMeeting(meetingId)
+  dialogDelete.value = false
 }
 const editMeeting = (meetingId) => {
-  router.push(`/meeting/${meetingId}`);
+  router.push(`/meeting/${meetingId}`)
 }
 </script>
